ada601fb0f7046215a746859b17cec52325b1d81
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2023-09-03 17:51:30 -0300

    index msg

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoices/Invoice/index.tsx";
 import InvoicesIndex from "./routes/Invoices/invoicesIndex/index.tsx";
+import IndexMsg from "./routes/IndexMsg/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
+                <Route index element={<IndexMsg/>} />
                 <Route path="expenses" element={<Expenses />} />
                 <Route path="invoices" element={<Invoices />}>
                     <Route index element={ <InvoicesIndex />} />
---
+++ src/routes/IndexMsg/index.tsx
@@ -0,0 +1,8 @@
+export default function IndexMsg() {
+    return(
+        <main>
+            <h2>Welcome!</h2>
+
+        </main>
+    )
+}
